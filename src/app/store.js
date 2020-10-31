663a4e64f46a82e75111d366e572fd90912d2c43
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-31 18:11:56 -0400

    refactor: change to using default reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import pokemonsSlice from '../features/Pokemons/pokemons.slice';
+import pokemonsReducer from '../features/Pokemons/pokemons.slice';
 
 const store = configureStore({
   reducer: {
-    pokemons: pokemonsSlice.reducer,
+    pokemons: pokemonsReducer,
   },
 });
 
