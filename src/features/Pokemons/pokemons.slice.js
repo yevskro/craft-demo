c52bac9dab3775e563411148daacc773c387020b
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-31 16:40:36 -0400

    refactor: change to new getPokemons api call using count

--- src/features/Pokemons/pokemons.slice.js
+++ src/features/Pokemons/pokemons.slice.js
@@ -3,30 +3,40 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { getPokemons } from '../../shared/api';
 
 const initialState = {
-  data: { count: 0 },
-  status: 'idle',
-  error: null,
+  data: {
+    count: 0,
+  } /* data will represent pokemons, count will keep track of pokemons in the data */,
+  fetch: { status: 'idle', error: null } /* fetch info */,
 };
 
 const fetchPokemons = createAsyncThunk(
   'pokemons/fetchPokemons',
   // eslint-disable-next-line no-unused-vars
   async (_, thunkApi) => {
-    const { count } = thunkApi.getState().pokemons.data;
+    const {
+      count,
+    } = thunkApi.getState().pokemons.data; /* get the current count */
     let pokemons;
+
     if (count === 150) {
-      pokemons = await getPokemons(150, 151);
+      /* if the count is 150 then we only need to get one more */
+      pokemons = await getPokemons(151, 1);
       return { ...pokemons, count: 151 };
     }
 
-    pokemons = await getPokemons(count + 1, count + 10);
+    /* count works in relation with pokemon id, the
+      starting index of the pokemon we get is count + 1,
+      and we wish to download the next 10 */
+    pokemons = await getPokemons(count + 1, 10);
     return { ...pokemons, count: count + 10 };
   }
 );
 
+/* data accessors */
 const selectAllPokemons = (state) => state.pokemons.data;
 const selectPokemonsCount = (state) => state.pokemons.data.count;
-const selectFetchStatus = (state) => state.pokemons.status;
+const selectFetchStatus = (state) => state.pokemons.fetch.status;
+const selectFetchError = (state) => state.pokemons.fetch.error;
 // const selectPokemonById = (state, id) => 1;
 
 const pokemonsSlice = createSlice({
@@ -49,4 +59,9 @@ const pokemonsSlice = createSlice({
 
 export default pokemonsSlice;
 export { fetchPokemons };
-export { selectAllPokemons, selectFetchStatus, selectPokemonsCount };
+export {
+  selectAllPokemons,
+  selectPokemonsCount,
+  selectFetchStatus,
+  selectFetchError,
+};
