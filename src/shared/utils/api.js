60ccfa05579441bd99a5b183bd37524c7e48e5a3
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-01 18:41:06 -0500

    feat: add lorem ipsum to description

--- src/shared/utils/api.js
+++ src/shared/utils/api.js
@@ -33,7 +33,8 @@ async function getPokemon(id) {
   pokemon[id].height = dataPokeAPI.height;
   pokemon[id].types = dataPokeAPI.types;
   pokemon[id].abilities = dataPokeAPI.abilities;
-  pokemon[id].description = 'durran durran';
+  pokemon[id].description =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
   const responseSanDiegoAPI = await fetch(`${SAN_DIEGO_MAP_API_URL}/${id}`, {
     method: 'GET',
