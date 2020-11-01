306298985fea63aa23fdef2fcca2c5685903e458
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-01 12:35:42 -0500

    feat: add getPokemon for detailed information

--- src/shared/utils/api.js
+++ src/shared/utils/api.js
@@ -1,17 +1,49 @@
-// eslint-disable-next-line no-unused-vars
-const SAN_DEIGO_MAP_API_URL = 'https://api.craft-demo.net/pokemon';
+const SAN_DIEGO_MAP_API_URL = 'https://api.craft-demo.net/pokemon';
 const POKEMON_API_KEY = 'HHko9Fuxf293b3w56zAJ89s3IcO9D5enaEPIg86l';
-const POKEMON_API_URL = 'https://pokeapi.co/api/v2/pokemon-form';
+const POKEMON_API_SPRITE_URL = 'https://pokeapi.co/api/v2/pokemon-form';
+const POKEMON_API_DETAIL_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 const DEFAULT_HEADERS = {
   'content-type': 'application/json',
 };
 
 // eslint-disable-next-line no-unused-vars
-const SAN_DIEGO_DEFAULT_HEADERS = {
+const SAN_DIEGO_HEADERS = {
   ...DEFAULT_HEADERS,
   'x-api-key': POKEMON_API_KEY,
 };
+
+async function getPokemon(id) {
+  /* 
+    this function will get a more detailed pokemon information
+  */
+  const pokemon = {};
+  const responsePokemonAPI = await fetch(`${POKEMON_API_DETAIL_URL}/${id}`, {
+    method: 'GET',
+    headers: DEFAULT_HEADERS,
+    mode: 'cors',
+  });
+  const dataPokeAPI = responsePokemonAPI.json();
+  pokemon[id] = {};
+  pokemon[id].name = dataPokeAPI.name;
+  pokemon[id].url = dataPokeAPI.sprites.front_default;
+  pokemon[id].weight = dataPokeAPI.weight;
+  pokemon[id].height = dataPokeAPI.height;
+  pokemon[id].types = dataPokeAPI.types;
+  pokemon[id].abilities = dataPokeAPI.abilities;
+  pokemon[id].skills = dataPokeAPI.moves;
+  pokemon[id].description = 'durran durran';
+
+  const responseSanDiegoAPI = await fetch(`${SAN_DIEGO_MAP_API_URL}/${id}`, {
+    method: 'GET',
+    headers: SAN_DIEGO_HEADERS,
+    mode: 'cors',
+  });
+
+  const dataMapAPI = responseSanDiegoAPI.json();
+  console.log(dataMapAPI);
+  return pokemon;
+}
 
 async function getPokemons(fromId, count) {
   /* 
@@ -22,10 +54,11 @@ async function getPokemons(fromId, count) {
   */
   const pokemons = {}; /* will hold the batched data */
   const promises = []; /* promises that will download the content */
+
   for (let id = fromId; id <= fromId + count; id += 1) {
     // eslint-disable-next-line no-await-in-loop
     promises.push(
-      fetch(`${POKEMON_API_URL}/${id}`, {
+      fetch(`${POKEMON_API_SPRITE_URL}/${id}`, {
         method: 'GET',
         headers: DEFAULT_HEADERS,
         mode: 'cors',
@@ -43,4 +76,4 @@ async function getPokemons(fromId, count) {
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export { getPokemons };
+export { getPokemons, getPokemon };
