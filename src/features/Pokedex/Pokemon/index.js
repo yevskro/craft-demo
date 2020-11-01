289a3764d6ffdd710519a335e3631bab10fb281c
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-01 12:36:39 -0500

    add comment

--- src/features/Pokedex/Pokemon/index.js
+++ src/features/Pokedex/Pokemon/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function Pokemon() {
+  /* using useParams for url slug id */
   const { id } = useParams();
   return <>{id}</>;
 }
