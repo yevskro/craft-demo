b3574b379079b31e0ee60f8ee866b4b6a703915d
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-01 07:03:28 -0500

    feat: add SwitchContainer for centering and giving top margin for Switch

--- src/features/Pokedex/index.js
+++ src/features/Pokedex/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import Switch from '../../shared/components/Switch';
 
 function Pokedex() {
@@ -12,15 +13,22 @@ function Pokedex() {
 
   return (
     <>
-      <Switch
-        left="All"
-        right="Bag"
-        onLeftSelected={handleAll}
-        onRightSelected={handleBag}
-      />
-      <Toggle />
+      <SwitchContainer>
+        <Switch
+          left="All"
+          right="Bag"
+          onLeftSelected={handleAll}
+          onRightSelected={handleBag}
+        />
+      </SwitchContainer>
     </>
   );
 }
+
+const SwitchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+`;
 
 export default Pokedex;
