6a38eb25246fbdfe01fd383c6117c3b92d53d5ff
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-01 11:23:15 -0500

    feat: add routes

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Pokedex from '../features/Pokedex';
+import Pokemon from '../features/Pokedex/Pokemon';
 
 function App() {
-  return <Pokedex />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/pokemon/:id">
+          <Pokemon />
+        </Route>
+        <Route path="/">
+          <Pokedex />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
