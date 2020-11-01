f68e07e628614542e9c5659afa325716e06e8bbd
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-01 13:04:12 -0500

    feat: fetching pokemon details and location on load

--- src/features/Pokedex/Pokemon/index.js
+++ src/features/Pokedex/Pokemon/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectPokemon, fetchPokemon } from '../pokedex.slice';
 
 function Pokemon() {
   /* using useParams for url slug id */
   const { id } = useParams();
-  return <>{id}</>;
+  const dispatch = useDispatch();
+  const pokemon = useSelector(selectPokemon);
+
+  useEffect(() => {
+    dispatch(fetchPokemon(id));
+  }, [id, dispatch]);
+
+  return <>{JSON.stringify(pokemon)}</>;
 }
 
 export default Pokemon;
