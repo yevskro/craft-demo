4e31748052bd2ea1885fa1f09c9eaca67094e4c9
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-01 10:54:44 -0500

    feat: add using PokemonCards and PokemonCardPlaceholers

--- src/features/pokedex/Pokemons/index.js
+++ src/features/pokedex/Pokemons/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
+import PokemonCard from '../PokemonCard';
+import PokemonPlaceholder from '../PokemonPlaceholder';
+
 import {
   selectPokemonsCount,
   selectSearchPokemonsResults,
@@ -9,7 +12,7 @@ import {
   fetchPokemons,
 } from '../pokedex.slice';
 
-function Pokemons() {
+function Pokemons({ bagged }) {
   const dispatch = useDispatch();
   const pokemonsCount = useSelector(selectPokemonsCount);
   const searchResults = useSelector(selectSearchPokemonsResults);
@@ -20,10 +23,12 @@ function Pokemons() {
       downloads the appropriate amount of 
       batches of pokemon info(name, url) 
     */
-    if (pokemonsCount !== 151) {
+    if (bagged) {
+      // place holder for reading bagged pokemons
+    } else if (pokemonsCount !== 151) {
       dispatch(fetchPokemons());
     }
-  }, [pokemonsCount, dispatch]);
+  }, [pokemonsCount, bagged, dispatch]);
 
   function pokemonsElements() {
     let from = 1;
@@ -39,43 +44,50 @@ function Pokemons() {
     for (let idx = from; idx <= to; idx += 1) {
       if (keys) {
         if (searchResults[keys[idx]]) {
-          pokemons.push(
-            <img
-              src={searchResults[keys[idx]].url}
-              key={keys[idx]}
-              alt=""
-              loading="lazy"
-            />
-          );
+          const { url, name } = searchResults[keys[idx]];
+          pokemons.push(<PokemonCard url={url} name={name} />);
         }
       } else if (searchResults[idx]) {
-        pokemons.push(
-          <img src={searchResults[idx].url} key={idx} alt="" loading="lazy" />
-        );
-      } else pokemons.push(<PokemonPlaceholder key={idx} />);
+        const { url, name } = searchResults[idx];
+        pokemons.push(<PokemonCard url={url} name={name} />);
+      } else pokemons.push(<PokemonPlaceholder />);
     }
     return pokemons;
   }
 
   return (
     <>
-      <input
-        type="text"
-        onChange={(e) => dispatch(searchPokemonsByNamePrefix(e.target.value))}
-        value={searchPokemonsNamePrefix}
-      />
-      <br />
-      {pokemonsElements()}
+      <SearchContainer>
+        <input
+          type="text"
+          onChange={(e) => dispatch(searchPokemonsByNamePrefix(e.target.value))}
+          value={searchPokemonsNamePrefix}
+        />
+      </SearchContainer>
+      <PokemonContainer>{bagged || pokemonsElements()}</PokemonContainer>
     </>
   );
 }
 
-const PokemonPlaceholder = styled.div`
-  display: inline-block;
-  width: 94px;
-  height: 94px;
-  border: 1px solid white;
-  background-color: rgba(225, 225, 225, 0.8);
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 25px;
+`;
+
+const PokemonContainer = styled.div`
+  margin-top: 10px;
+  text-align: center;
+
+  @media (min-width: 400px) {
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+
+  @media (min-width: 700px) {
+    margin-left: 11%;
+    margin-right: 11%;
+  }
 `;
 
 export default Pokemons;
