188b0436613193cb34141b9b75ad90131b753933
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-02 08:56:04 -0500

    add commentary

--- src/shared/components/Search/index.js
+++ src/shared/components/Search/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Search({ search, onSearch }) {
+  /* onSearch computes search */
   return (
     <SearchContainer>
       <SearchIcon src="/search.png" alt="search icon" />
