c583ed071a0d3d6e9653f8664ce6240e4f69f127
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-11-02 00:38:20 -0500

    feat: add asyncs methods to handle local storage actions

--- src/features/Pokedex/BaggedPokemons/pokebag.slice.js
+++ src/features/Pokedex/BaggedPokemons/pokebag.slice.js
@@ -3,24 +3,40 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { filterPokemonsByNamePrefix } from '../../../shared/utils/pokedex';
 
 const initialState = {
-  baggedPokemons: {
+  pokemons: {
     count: 0,
   },
-  searchBaggedPokemons: { results: {}, namePrefix: '' },
-  storage: { status: 'idle', error: null },
+  searchpokemons: { results: {}, namePrefix: '' },
+  load: { status: 'idle', error: null },
+  add: { status: 'idle', error: null },
+  remove: { status: 'idle', error: null },
 };
 
 const loadPokemons = createAsyncThunk('pokebag/loadPokemon', async () => {
-  return { test: 'test' };
+  const data = JSON.parse(window.localStorage.getItem('pokebag'));
+  return { pokebag: data };
 });
 
-const addPokemon = createAsyncThunk('pokebag/addPokemon', async (id) => {
-  return { test: 'test' };
-});
+const addPokemon = createAsyncThunk(
+  'pokebag/addPokemon',
+  async (pokemon, thunkAPI) => {
+    let { pokemons } = thunkAPI.getState().pokebag;
+    pokemons = { ...pokemons, pokemon };
+    window.localStorage.setItem('pokebag', JSON.stringify(pokemons));
+    return pokemons;
+  }
+);
 
-const removePokemon = createAsyncThunk('pokebag/removePokemon', async (id) => {
-  return { test: 'test' };
-});
+const removePokemon = createAsyncThunk(
+  'pokebag/removePokemon',
+  async (id, thunkAPI) => {
+    let { pokemons } = thunkAPI.getState().pokebag;
+    pokemons = { ...pokemons };
+    delete pokemons[id];
+    window.localStorage.setItem('pokebag', JSON.stringify(pokemons));
+    return pokemons;
+  }
+);
 
 /* data selectors */
 const selectAllPokemons = (state) => state.pokebag.pokemons;
@@ -46,10 +62,10 @@ const pokebagSlice = createSlice({
   },
   extraReducers: {
     [loadPokemons.pending]: (state) => {
-      state.status = 'loading';
+      state.load.status = 'loading';
     },
     [loadPokemons.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
+      state.load.status = 'succeeded';
       state.pokemons = { ...state.pokemons, ...action.payload };
       state.searchPokemons.results = filterPokemonsByNamePrefix(
         state.pokemons,
@@ -57,39 +73,38 @@ const pokebagSlice = createSlice({
       );
     },
     [loadPokemons.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
+      state.load.status = 'failed';
+      state.load.error = action.error.message;
     },
     [addPokemon.pending]: (state) => {
-      state.status = 'loading';
+      state.add.status = 'loading';
     },
     [addPokemon.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
-      state.pokemons = { ...state.pokemons, ...action.payload };
+      state.add.status = 'succeeded';
+      state.pokemons = action.payload;
       state.searchPokemons.results = filterPokemonsByNamePrefix(
         state.pokemons,
         state.searchPokemons.namePrefix
       );
     },
     [addPokemon.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
+      state.add.status = 'failed';
+      state.add.error = action.error.message;
     },
     [removePokemon.pending]: (state) => {
-      state.status = 'loading';
+      state.remove.status = 'loading';
     },
     [removePokemon.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
-      state.pokemons = { ...state.pokemons };
-      delete state.pokemons[action.payload];
+      state.remove.status = 'succeeded';
+      state.pokemons = action.payload;
       state.searchPokemons.results = filterPokemonsByNamePrefix(
         state.pokemons,
         state.searchPokemons.namePrefix
       );
     },
     [removePokemon.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
+      state.remove.status = 'failed';
+      state.remove.error = action.error.message;
     },
   },
 });
